cabf1b575c10ff1c9854796a4dd822fb300ea958
ari-gunawan <63304855+ari-gunawan@users.noreply.github.com>
2021-10-04 20:37:33 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="styles.css">
+    <title>upj.ac.id/pmb</title>
+</head>
+
+<body>
+    <div class="top-head header-7" data-stickey="true">
+        <div class="counteiner">
+            <div class="row">
+                <div class="col-sm-4 align-middle">
+                    <div class="logo">
+                        <a href="upj.ac.id/">
+                            <img alt="logo" src="images/logo_upj.png">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="media-bottom">
+                        <br>
+                        <h21 class="tittle-topa inter" style="font-size:18px;">INTEGRITY</h21>
+                        <br>
+                        <h21 class="tittle-topb profe" style="font-size:18px;">PROFESSIONALISM</h21>
+                        <br>
+                        <h21 class="tittle-topc entre" style="font-size:18px;">ENTREPRENEURSHIP</h21>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+</body>
+
+</html>
